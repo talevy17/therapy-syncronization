c8b648c16f3009520f17312e8b4182d1fff2cff9
Tal Levy <tall@wix.com>
2020-05-09 13:19:59 +0300

    demo upload and download data scheme

--- api/api.py
+++ api/api.py
@@ -1,14 +1,11 @@
 
 import os
-from flask import Flask, flash, request, redirect, url_for, session
+from flask import Flask, request, redirect, url_for, send_from_directory, current_app
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
-import logging
 
-logging.basicConfig(level=logging.INFO)
-
-logger = logging.getLogger('HELLO WORLD')
 UPLOAD_FOLDER = 'files'
-ALLOWED_EXTENSIONS = set(['txt', 'csv', 'xlsx', 'xlsb', 'xlsm', 'xls'])
+ALLOWED_EXTENSIONS = {'txt', 'csv', 'xlsx', 'xlsb', 'xlsm', 'xls'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -16,19 +13,23 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/upload', methods=['POST'])
 def file_upload():
-    target = os.path.join(UPLOAD_FOLDER, 'test_docs')
-    if not os.path.isdir(target):
-        os.mkdir(target)
-    logger.info("welcome to upload`")
     file = request.files['file']
     filename = secure_filename(file.filename)
-    destination = "/".join([target, filename])
-    file.save(destination)
-    # session['uploadFilePath'] = destination
-    response = "Whatever you wish too return"
-    return response
+    file.save(os.path.join(UPLOAD_FOLDER, filename))
+
+    # process data and send
+
+    return redirect(url_for('download', filename=filename))
+
+
+@app.route('/files/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    path = os.path.abspath(os.path.join(current_app.root_path, os.pardir))
+    return send_from_directory(directory=os.path.join(path, app.config['UPLOAD_FOLDER']), filename=filename, as_attachment=True)
 
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
     app.run(debug=True, host="0.0.0.0", use_reloader=False)
+
+CORS(app, expose_headers='Authorization')
--- demo/src/Upload.jsx
+++ demo/src/Upload.jsx
@@ -1,46 +1,51 @@
 import React, { Component } from "react";
 
 export default class Upload extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { upload: false };
+  }
   handleFileUpload = (e) => {
     e.preventDefault();
 
     const data = new FormData();
     data.append("file", this.uploadInput.files[0]);
-    data.append("filename", this.fileName.value);
-
+    this.setState({ upload: true });
     fetch("/upload", {
       method: "POST",
       body: data,
     }).then((response) => {
-      console.log(response);
+      this.setState(
+        { upload: false },
+        () => (window.location.href = response.url)
+      );
     });
+  };
+
+  renderFileReady = () => {
+    return this.state && this.state.upload ? (
+      <div> Proccessing Request... </div>
+    ) : null;
   };
 
   render() {
     return (
-      <form onSubmit={this.handleFileUpload}>
-        <div>
-          <input
-            ref={(ref) => {
-              this.uploadInput = ref;
-            }}
-            type="file"
-          />
-        </div>
-        <div>
-          <input
-            ref={(ref) => {
-              this.fileName = ref;
-            }}
-            type="text"
-            placeholder="Enter the desired name of file"
-          />
-        </div>
-        <br />
-        <div>
-          <button>Upload</button>
-        </div>
-      </form>
+      <div>
+        <form onSubmit={this.handleFileUpload}>
+          <div>
+            <input
+              ref={(ref) => {
+                this.uploadInput = ref;
+              }}
+              type="file"
+            />
+          </div>
+          <div>
+            <button>Upload</button>
+          </div>
+        </form>
+        {this.renderFileReady()}
+      </div>
     );
   }
 }
